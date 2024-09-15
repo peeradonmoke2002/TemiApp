b3df1f155a782ed11a242ffbad19384ac60ec129
peeradonmoke2002 <peeradonmoke2002@gmail.com>
2024-09-16 00:33:06 +0700

    Add handle message store

--- app/src/main/java/com/example/temiapp/network/RabbitMQService.kt
+++ app/src/main/java/com/example/temiapp/network/RabbitMQService.kt
@@ -3,14 +3,18 @@ package com.example.temiapp.network
 import android.app.Service
 import android.content.Intent
 import android.os.Binder
+import android.os.Handler
 import android.os.IBinder
+import android.os.Looper
 import android.util.Log
 import com.example.temiapp.controller.RobotController
+import com.example.temiapp.ui.LoadingActivity
 import com.robotemi.sdk.Robot
 
 class RabbitMQService : Service() {
 
     private lateinit var rabbitMQClient: RabbitMQClient
+
     private val binder = RabbitBinder()
 
     // Binder class to return the instance of RabbitMQService
@@ -27,7 +31,8 @@ class RabbitMQService : Service() {
 
         // Initialize RabbitMQ client with queue handlers
         val queueHandlers = mapOf(
-            "robot_control_queue" to { message: String -> handleRabbitMqMessage(message) }
+            "robot_control_queue" to { message: String -> handleRabbitMqMessageController(message) },
+//            "store_update_queue" to { message: String -> handleRabbitMqMessage(message)}
         )
 
         rabbitMQClient = RabbitMQClient(
@@ -47,13 +52,14 @@ class RabbitMQService : Service() {
     }
 
     // Example of handling RabbitMQ messages in the service
-    private fun handleRabbitMqMessage(message: String) {
+    private fun handleRabbitMqMessageController(message: String) {
         Log.d("RabbitMQService", "Received message: $message")
         // Process the message (e.g., controlling the robot via RobotController)
         val robot = Robot.getInstance()
         val controller = RobotController(robot)
         controller.handleRabbitMqMessage(message)
     }
+
 
     // Method to stop RabbitMQ connection manually
     fun stopRabbitMQ() {
